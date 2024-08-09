94d59baba978687a610da347e06a19cbc16ead40
czz_y <czybove@qq.com>
2024-08-09 17:45:47 +0800

    first commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,410 @@
+﻿namespace AntdUIDemo
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            AntdUI.MenuItem menuItem1 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem2 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem3 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem4 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem5 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem6 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem7 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem8 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem9 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem10 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem11 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem12 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem13 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem14 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem15 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem16 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem17 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem18 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem19 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem20 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem21 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem22 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem23 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem24 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem25 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem26 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem27 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem28 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem29 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem30 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem31 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem32 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem33 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem34 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem35 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem36 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem37 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem38 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem39 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem40 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem41 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem42 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem43 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem44 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem45 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem46 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem47 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem48 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem49 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem50 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem51 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem52 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem53 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem54 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem55 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem56 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem57 = new AntdUI.MenuItem();
+            AntdUI.MenuItem menuItem58 = new AntdUI.MenuItem();
+            this.titlebar = new AntdUI.WindowBar();
+            this.button1 = new AntdUI.Button();
+            this.divider1 = new AntdUI.Divider();
+            this.menu1 = new AntdUI.Menu();
+            this.titlebar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titlebar
+            // 
+            this.titlebar.Controls.Add(this.button1);
+            this.titlebar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titlebar.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.titlebar.Location = new System.Drawing.Point(12, 12);
+            this.titlebar.MaximizeBox = false;
+            this.titlebar.Name = "titlebar";
+            this.titlebar.Size = new System.Drawing.Size(984, 40);
+            this.titlebar.SubText = "Demo";
+            this.titlebar.TabIndex = 0;
+            this.titlebar.Text = "AntdUI";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Ghost = true;
+            this.button1.ImageSvg = resources.GetString("button1.ImageSvg");
+            this.button1.Location = new System.Drawing.Point(838, 0);
+            this.button1.Name = "button1";
+            this.button1.Radius = 0;
+            this.button1.Size = new System.Drawing.Size(50, 40);
+            this.button1.TabIndex = 0;
+            this.button1.WaveSize = 0;
+            // 
+            // divider1
+            // 
+            this.divider1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.divider1.Location = new System.Drawing.Point(12, 52);
+            this.divider1.Name = "divider1";
+            this.divider1.Size = new System.Drawing.Size(984, 12);
+            this.divider1.TabIndex = 8;
+            // 
+            // menu1
+            // 
+            this.menu1.AutoCollapse = true;
+            this.menu1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menu1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.menu1.Indent = true;
+            menuItem1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            menuItem1.PARENTITEM = null;
+            menuItem1.Select = false;
+            menuItem2.PARENTITEM = menuItem1;
+            menuItem2.Select = false;
+            menuItem2.Text = "Button 按钮";
+            menuItem3.PARENTITEM = menuItem1;
+            menuItem3.Select = false;
+            menuItem3.Text = "FloatButton 悬浮按钮";
+            menuItem1.Sub.Add(menuItem2);
+            menuItem1.Sub.Add(menuItem3);
+            menuItem1.Text = "通用";
+            menuItem4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem4.PARENTITEM = null;
+            menuItem4.Select = false;
+            menuItem5.PARENTITEM = menuItem4;
+            menuItem5.Select = false;
+            menuItem5.Text = "FlowPanel 流动布局";
+            menuItem6.PARENTITEM = menuItem4;
+            menuItem6.Select = false;
+            menuItem6.Text = "GridPanel 格栅布局";
+            menuItem7.PARENTITEM = menuItem4;
+            menuItem7.Select = false;
+            menuItem7.Text = "StackPanel 堆栈布局";
+            menuItem4.Sub.Add(menuItem5);
+            menuItem4.Sub.Add(menuItem6);
+            menuItem4.Sub.Add(menuItem7);
+            menuItem4.Text = "布局";
+            menuItem8.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem8.PARENTITEM = null;
+            menuItem8.Select = false;
+            menuItem9.PARENTITEM = menuItem8;
+            menuItem9.Select = false;
+            menuItem9.Text = "Dropdown 下拉菜单";
+            menuItem10.PARENTITEM = menuItem8;
+            menuItem10.Select = false;
+            menuItem10.Text = "Menu 导航菜单";
+            menuItem11.PARENTITEM = menuItem8;
+            menuItem11.Select = false;
+            menuItem11.Text = "Pagination 分页";
+            menuItem12.PARENTITEM = menuItem8;
+            menuItem12.Select = false;
+            menuItem12.Text = "Steps 步骤条";
+            menuItem8.Sub.Add(menuItem9);
+            menuItem8.Sub.Add(menuItem10);
+            menuItem8.Sub.Add(menuItem11);
+            menuItem8.Sub.Add(menuItem12);
+            menuItem8.Text = "导航";
+            menuItem13.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem13.PARENTITEM = null;
+            menuItem13.Select = false;
+            menuItem14.PARENTITEM = menuItem13;
+            menuItem14.Select = false;
+            menuItem14.Text = "Checkbox 多选框";
+            menuItem15.PARENTITEM = menuItem13;
+            menuItem15.Select = false;
+            menuItem15.Text = "ColorPicker 颜色选择器";
+            menuItem16.PARENTITEM = menuItem13;
+            menuItem16.Select = false;
+            menuItem16.Text = "DatePicker 日期选择框";
+            menuItem17.PARENTITEM = menuItem13;
+            menuItem17.Select = false;
+            menuItem17.Text = "DatePickerRange 日期范围选择框";
+            menuItem18.PARENTITEM = menuItem13;
+            menuItem18.Select = false;
+            menuItem18.Text = "Input 输入框";
+            menuItem19.PARENTITEM = menuItem13;
+            menuItem19.Select = false;
+            menuItem19.Text = "InputNumber 数字输入框";
+            menuItem20.PARENTITEM = menuItem13;
+            menuItem20.Select = false;
+            menuItem20.Text = "Radio 单选框";
+            menuItem21.PARENTITEM = menuItem13;
+            menuItem21.Select = false;
+            menuItem21.Text = "Rate 评分";
+            menuItem22.PARENTITEM = menuItem13;
+            menuItem22.Select = false;
+            menuItem22.Text = "Select 选择器";
+            menuItem23.PARENTITEM = menuItem13;
+            menuItem23.Select = false;
+            menuItem23.Text = "Slider 滑动输入条";
+            menuItem24.PARENTITEM = menuItem13;
+            menuItem24.Select = false;
+            menuItem24.Text = "Switch 开关";
+            menuItem25.PARENTITEM = menuItem13;
+            menuItem25.Select = false;
+            menuItem25.Text = "TimePicker 时间选择框";
+            menuItem13.Sub.Add(menuItem14);
+            menuItem13.Sub.Add(menuItem15);
+            menuItem13.Sub.Add(menuItem16);
+            menuItem13.Sub.Add(menuItem17);
+            menuItem13.Sub.Add(menuItem18);
+            menuItem13.Sub.Add(menuItem19);
+            menuItem13.Sub.Add(menuItem20);
+            menuItem13.Sub.Add(menuItem21);
+            menuItem13.Sub.Add(menuItem22);
+            menuItem13.Sub.Add(menuItem23);
+            menuItem13.Sub.Add(menuItem24);
+            menuItem13.Sub.Add(menuItem25);
+            menuItem13.Text = "数据录入";
+            menuItem26.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem26.PARENTITEM = null;
+            menuItem26.Select = false;
+            menuItem27.PARENTITEM = menuItem26;
+            menuItem27.Select = false;
+            menuItem27.Text = "Avatar 头像";
+            menuItem28.PARENTITEM = menuItem26;
+            menuItem28.Select = false;
+            menuItem28.Text = "Badge 徽标数";
+            menuItem29.PARENTITEM = menuItem26;
+            menuItem29.Select = false;
+            menuItem29.Text = "Calendar 日历";
+            menuItem30.PARENTITEM = menuItem26;
+            menuItem30.Select = false;
+            menuItem30.Text = "Carousel 走马灯";
+            menuItem31.PARENTITEM = menuItem26;
+            menuItem31.Select = false;
+            menuItem31.Text = "Lable 标签";
+            menuItem32.PARENTITEM = menuItem26;
+            menuItem32.Select = false;
+            menuItem32.Text = "Panel 面板";
+            menuItem33.PARENTITEM = menuItem26;
+            menuItem33.Select = false;
+            menuItem33.Text = "Popover 气泡卡片";
+            menuItem34.PARENTITEM = menuItem26;
+            menuItem34.Select = false;
+            menuItem34.Text = "Preview 图片预览";
+            menuItem35.PARENTITEM = menuItem26;
+            menuItem35.Select = false;
+            menuItem35.Text = "Segmented 分段控制器";
+            menuItem36.PARENTITEM = menuItem26;
+            menuItem36.Select = false;
+            menuItem36.Text = "Table 表格";
+            menuItem37.PARENTITEM = menuItem26;
+            menuItem37.Select = false;
+            menuItem37.Text = "Tabs 标签页";
+            menuItem38.PARENTITEM = menuItem26;
+            menuItem38.Select = false;
+            menuItem38.Text = "Tag 标签";
+            menuItem39.PARENTITEM = menuItem26;
+            menuItem39.Select = false;
+            menuItem39.Text = "Timeline 时间轴";
+            menuItem40.PARENTITEM = menuItem26;
+            menuItem40.Select = false;
+            menuItem40.Text = "Tooltip 文字提示";
+            menuItem41.PARENTITEM = menuItem26;
+            menuItem41.Select = false;
+            menuItem41.Text = "Tree 树形控件";
+            menuItem26.Sub.Add(menuItem27);
+            menuItem26.Sub.Add(menuItem28);
+            menuItem26.Sub.Add(menuItem29);
+            menuItem26.Sub.Add(menuItem30);
+            menuItem26.Sub.Add(menuItem31);
+            menuItem26.Sub.Add(menuItem32);
+            menuItem26.Sub.Add(menuItem33);
+            menuItem26.Sub.Add(menuItem34);
+            menuItem26.Sub.Add(menuItem35);
+            menuItem26.Sub.Add(menuItem36);
+            menuItem26.Sub.Add(menuItem37);
+            menuItem26.Sub.Add(menuItem38);
+            menuItem26.Sub.Add(menuItem39);
+            menuItem26.Sub.Add(menuItem40);
+            menuItem26.Sub.Add(menuItem41);
+            menuItem26.Text = "数据展示";
+            menuItem42.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem42.PARENTITEM = null;
+            menuItem42.Select = false;
+            menuItem43.PARENTITEM = menuItem42;
+            menuItem43.Select = false;
+            menuItem43.Text = "Alert 警告提示";
+            menuItem44.PARENTITEM = menuItem42;
+            menuItem44.Select = false;
+            menuItem44.Text = "Drawer 抽屉";
+            menuItem45.PARENTITEM = menuItem42;
+            menuItem45.Select = false;
+            menuItem45.Text = "Message 全局提示";
+            menuItem46.PARENTITEM = menuItem42;
+            menuItem46.Select = false;
+            menuItem46.Text = "Modal 对话框";
+            menuItem47.PARENTITEM = menuItem42;
+            menuItem47.Select = false;
+            menuItem47.Text = "Notification 通知提醒框";
+            menuItem48.PARENTITEM = menuItem42;
+            menuItem48.Select = false;
+            menuItem48.Text = "Progress 进度条";
+            menuItem49.PARENTITEM = menuItem42;
+            menuItem49.Select = false;
+            menuItem49.Text = "Spin 加载中";
+            menuItem42.Sub.Add(menuItem43);
+            menuItem42.Sub.Add(menuItem44);
+            menuItem42.Sub.Add(menuItem45);
+            menuItem42.Sub.Add(menuItem46);
+            menuItem42.Sub.Add(menuItem47);
+            menuItem42.Sub.Add(menuItem48);
+            menuItem42.Sub.Add(menuItem49);
+            menuItem42.Text = "反馈";
+            menuItem50.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem50.PARENTITEM = null;
+            menuItem50.Select = false;
+            menuItem51.PARENTITEM = menuItem50;
+            menuItem51.Select = false;
+            menuItem51.Text = "MsgList 好友消息列表";
+            menuItem52.PARENTITEM = menuItem50;
+            menuItem52.Select = false;
+            menuItem52.Text = "ChatList 气泡聊天列表";
+            menuItem50.Sub.Add(menuItem51);
+            menuItem50.Sub.Add(menuItem52);
+            menuItem50.Text = "聊天";
+            menuItem53.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            menuItem53.PARENTITEM = null;
+            menuItem53.Select = false;
+            menuItem54.PARENTITEM = menuItem53;
+            menuItem54.Select = false;
+            menuItem54.Text = "Battery 电量";
+            menuItem55.PARENTITEM = menuItem53;
+            menuItem55.Select = false;
+            menuItem55.Text = "ContextMenuStrip 右键菜单";
+            menuItem56.PARENTITEM = menuItem53;
+            menuItem56.Select = false;
+            menuItem56.Text = "Divider 分割线";
+            menuItem57.PARENTITEM = menuItem53;
+            menuItem57.Select = false;
+            menuItem57.Text = "Image3D 图片3D";
+            menuItem58.PARENTITEM = menuItem53;
+            menuItem58.Select = false;
+            menuItem58.Text = "WindowBar 窗口栏";
+            menuItem53.Sub.Add(menuItem54);
+            menuItem53.Sub.Add(menuItem55);
+            menuItem53.Sub.Add(menuItem56);
+            menuItem53.Sub.Add(menuItem57);
+            menuItem53.Sub.Add(menuItem58);
+            menuItem53.Text = "其他";
+            this.menu1.Items.Add(menuItem1);
+            this.menu1.Items.Add(menuItem4);
+            this.menu1.Items.Add(menuItem8);
+            this.menu1.Items.Add(menuItem13);
+            this.menu1.Items.Add(menuItem26);
+            this.menu1.Items.Add(menuItem42);
+            this.menu1.Items.Add(menuItem50);
+            this.menu1.Items.Add(menuItem53);
+            this.menu1.Location = new System.Drawing.Point(12, 64);
+            this.menu1.Name = "menu1";
+            this.menu1.Size = new System.Drawing.Size(277, 548);
+            this.menu1.TabIndex = 9;
+            this.menu1.Text = "menu1";
+            this.menu1.Unique = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1008, 624);
+            this.ControlBox = false;
+            this.Controls.Add(this.menu1);
+            this.Controls.Add(this.divider1);
+            this.Controls.Add(this.titlebar);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Opacity = 0.96D;
+            this.Padding = new System.Windows.Forms.Padding(12);
+            this.Resizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.titlebar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AntdUI.WindowBar titlebar;
+        private AntdUI.Button button1;
+        private AntdUI.Divider divider1;
+        private AntdUI.Menu menu1;
+    }
+}
---
+++ packages/AntdUI.1.5.1/README.md
@@ -0,0 +1,89 @@
+# AntdUI
+Winform UI library use Ant Design 5.0
+
+### ✨特性
+
+- 🌈 纯GDI绘制，没有图片，支持AOT
+- 🎨 高质量绘图，高性能动画
+- 🚀 Winform上最阴影的阴影效果
+- 📦 无边框窗口，拥有原生窗口特性
+- 💎 3D翻转效果
+- 👚 主题配置
+- 🦜 SVG矢量图
+- 👓 DPI适配
+- 🌍 国际化
+
+### 🖥支持环境
+
+- .NET 6.0及以上。
+- .NET Framework4.8及以上。
+- .NET Framework4.0及以上。
+
+### 🌴控件
+
+:arrow_down: | 通用 `2` | 动画 | 禁用 |
+:---:|:--|:--:|:--:|
+:arrow_right: | **Button** 按钮 | ✅ | ✅ |
+:arrow_right: | **FloatButton** 悬浮按钮 | ✅ | ❎ |
+||||
+:arrow_down: | 布局 `4` | 动画 | 禁用 |
+:arrow_right: | **Divider** 分割线 | ❎ | ❎ |
+:arrow_right: | **StackPanel** 堆栈布局 | ❎ | ❎ |
+:arrow_right: | **FlowPanel** 流动布局 | ❎ | ❎ |
+:arrow_right: | **GridPanel** 格栅布局 | ❎ | ❎ |
+||||
+:arrow_down: | 导航 `4` | 动画 | 禁用 |
+:arrow_right: | **Dropdown** 下拉菜单 | ✅ | ✅ |
+:arrow_right: | **Menu** 导航菜单 | ✅ | ❎ |
+:arrow_right: | **Pagination** 分页 | ✅ | ✅ |
+:arrow_right: | **Steps** 步骤条 | ❎ | ❎ |
+||||
+:arrow_down: | 数据录入 `12` | 动画 | 禁用 |
+:arrow_right: | **Checkbox** 多选框 | ✅ | ✅ |
+:arrow_right: | **ColorPicker** 颜色选择器 | ✅ | ✅ |
+:arrow_right: | **DatePicker** 日期选择框 | ✅ | ✅ |
+:arrow_right: | **DatePickerRange** 日期范围选择框 | ✅ | ✅ |
+:arrow_right: | **Input** 输入框 | ✅ | ✅ |
+:arrow_right: | **InputNumber** 数字输入框 | ✅ | ✅ |
+:arrow_right: | **Radio** 单选框 | ✅ | ✅ |
+:arrow_right: | **Rate** 评分 | ✅ | ❎ |
+:arrow_right: | **Select** 选择器 | ✅ | ✅ |
+:arrow_right: | **Slider** 滑动输入条 | ✅ | ❎ |
+:arrow_right: | **Switch** 开关 | ✅ | ✅ |
+:arrow_right: | **TimePicker** 时间选择框 | ✅ | ✅ |
+||||
+:arrow_down: | 数据展示 `15` | 动画 | 禁用 |
+:arrow_right: | **Avatar** 头像 | ❎ | ❎ |
+:arrow_right: | **Badge** 徽标数 | ✅ | ❎ |
+:arrow_right: | **Calendar** 日历 | ✅ | ❎ |
+:arrow_right: | **Panel** 面板 | ✅ | ❎ |
+:arrow_right: | **Carousel** 走马灯 | ✅ | ❎ |
+:arrow_right: | **Preview** 图片预览 | ✅ | ✅ |
+:arrow_right: | **Popover** 气泡卡片 | ✅ | ❎ |
+:arrow_right: | **Segmented** 分段控制器 | ✅ | ❎ |
+:arrow_right: | **Table** 表格 | ✅ | ❎ |
+:arrow_right: | **Tabs** 标签页 | ✅ | ❎ |
+:arrow_right: | **Tag** 标签 | ✅ | ❎ |
+:arrow_right: | **Timeline** 时间轴 | ❎ | ❎ |
+:arrow_right: | **Tooltip** 文字提示 | ✅ | ❎ |
+:arrow_right: | **Tree** 树形控件 | ✅ | ✅ |
+:arrow_right: | **Lable** 标签 | ✅ | ❎ |
+||||
+:arrow_down: | 反馈 `7` | 动画 | 禁用 |
+:arrow_right: | **Alert** 警告提示 | ✅ | ❎ |
+:arrow_right: | **Drawer** 抽屉 | ✅ | ❎ |
+:arrow_right: | **Message** 全局提示 | ✅ | ❎ |
+:arrow_right: | **Modal** 对话框 | ✅ | ❎ |
+:arrow_right: | **Notification** 通知提醒框 | ✅ | ❎ |
+:arrow_right: | **Progress** 进度条 | ✅ | ❎ |
+:arrow_right: | **Spin** 加载中 | ✅ | ❎ |
+||||
+:arrow_down: | 聊天 `2` | 动画 | 禁用 |
+:arrow_right: | **MsgList** 好友消息列表 | ✅ | ❎ |
+:arrow_right: | **ChatList** 气泡聊天列表 | ✅ | ❎ |
+||||
+:arrow_down: | 其他 `4` | 动画 | 禁用 |
+:arrow_right: | **WindowBar** 窗口栏 | ✅ | ❎ |
+:arrow_right: | **Battery** 电量 | ✅ | ❎ |
+:arrow_right: | **ContextMenuStrip** 右键菜单 | ✅ | ❎ |
+:arrow_right: | **Image3D** 图片3D | ✅ | ❎ |
