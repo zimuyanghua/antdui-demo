348a27d3c33c5619a11687806d8aace0c5d67365
czz_y <czybove@qq.com>
2024-09-09 13:19:51 +0800

    Rate支持清除

--- Views/RateDemo.Designer.cs
+++ Views/RateDemo.Designer.cs
@@ -30,7 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RateDemo));
             this.stackPanel1 = new AntdUI.StackPanel();
+            this.flowPanel1 = new AntdUI.FlowPanel();
+            this.rate7 = new AntdUI.Rate();
+            this.rate2 = new AntdUI.Rate();
+            this.rate1 = new AntdUI.Rate();
+            this.label3 = new AntdUI.Label();
+            this.divider1 = new AntdUI.Divider();
+            this.label2 = new AntdUI.Label();
+            this.label1 = new AntdUI.Label();
+            this.label7 = new AntdUI.Label();
+            this.flowPanel5 = new AntdUI.FlowPanel();
+            this.rate9 = new AntdUI.Rate();
+            this.rate10 = new AntdUI.Rate();
+            this.rate11 = new AntdUI.Rate();
             this.flowPanel4 = new AntdUI.FlowPanel();
+            this.rate8 = new AntdUI.Rate();
             this.rate6 = new AntdUI.Rate();
             this.rate5 = new AntdUI.Rate();
             this.label6 = new AntdUI.Label();
@@ -40,20 +54,12 @@
             this.flowPanel2 = new AntdUI.FlowPanel();
             this.rate3 = new AntdUI.Rate();
             this.label4 = new AntdUI.Label();
-            this.flowPanel1 = new AntdUI.FlowPanel();
-            this.rate7 = new AntdUI.Rate();
-            this.rate2 = new AntdUI.Rate();
-            this.rate1 = new AntdUI.Rate();
-            this.label3 = new AntdUI.Label();
-            this.divider1 = new AntdUI.Divider();
-            this.label2 = new AntdUI.Label();
-            this.label1 = new AntdUI.Label();
-            this.rate8 = new AntdUI.Rate();
             this.stackPanel1.SuspendLayout();
+            this.flowPanel1.SuspendLayout();
+            this.flowPanel5.SuspendLayout();
             this.flowPanel4.SuspendLayout();
             this.flowPanel3.SuspendLayout();
             this.flowPanel2.SuspendLayout();
-            this.flowPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // stackPanel1
@@ -64,6 +70,8 @@
             this.stackPanel1.Controls.Add(this.label5);
             this.stackPanel1.Controls.Add(this.flowPanel2);
             this.stackPanel1.Controls.Add(this.label4);
+            this.stackPanel1.Controls.Add(this.flowPanel5);
+            this.stackPanel1.Controls.Add(this.label7);
             this.stackPanel1.Controls.Add(this.flowPanel1);
             this.stackPanel1.Controls.Add(this.label3);
             this.stackPanel1.Controls.Add(this.divider1);
@@ -72,111 +80,10 @@
             this.stackPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.stackPanel1.Location = new System.Drawing.Point(0, 0);
             this.stackPanel1.Name = "stackPanel1";
-            this.stackPanel1.Size = new System.Drawing.Size(700, 435);
+            this.stackPanel1.Size = new System.Drawing.Size(700, 494);
             this.stackPanel1.TabIndex = 0;
             this.stackPanel1.Text = "stackPanel1";
             this.stackPanel1.Vertical = true;
-            // 
-            // flowPanel4
-            // 
-            this.flowPanel4.Controls.Add(this.rate8);
-            this.flowPanel4.Controls.Add(this.rate6);
-            this.flowPanel4.Controls.Add(this.rate5);
-            this.flowPanel4.Location = new System.Drawing.Point(3, 349);
-            this.flowPanel4.Name = "flowPanel4";
-            this.flowPanel4.Size = new System.Drawing.Size(694, 38);
-            this.flowPanel4.TabIndex = 18;
-            this.flowPanel4.Text = "flowPanel4";
-            // 
-            // rate6
-            // 
-            this.rate6.Character = "A";
-            this.rate6.Location = new System.Drawing.Point(209, 3);
-            this.rate6.Name = "rate6";
-            this.rate6.Size = new System.Drawing.Size(200, 32);
-            this.rate6.TabIndex = 2;
-            this.rate6.Text = "rate6";
-            this.rate6.Value = 3F;
-            // 
-            // rate5
-            // 
-            this.rate5.Character = resources.GetString("rate5.Character");
-            this.rate5.Location = new System.Drawing.Point(3, 3);
-            this.rate5.Name = "rate5";
-            this.rate5.Size = new System.Drawing.Size(200, 32);
-            this.rate5.TabIndex = 1;
-            this.rate5.Text = "rate5";
-            this.rate5.Value = 3F;
-            // 
-            // label6
-            // 
-            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label6.Location = new System.Drawing.Point(3, 319);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(694, 24);
-            this.label6.TabIndex = 17;
-            this.label6.Text = "字符自定义";
-            // 
-            // flowPanel3
-            // 
-            this.flowPanel3.Controls.Add(this.rate4);
-            this.flowPanel3.Location = new System.Drawing.Point(3, 275);
-            this.flowPanel3.Name = "flowPanel3";
-            this.flowPanel3.Size = new System.Drawing.Size(694, 38);
-            this.flowPanel3.TabIndex = 16;
-            this.flowPanel3.Text = "flowPanel3";
-            // 
-            // rate4
-            // 
-            this.rate4.Location = new System.Drawing.Point(3, 3);
-            this.rate4.Name = "rate4";
-            this.rate4.Size = new System.Drawing.Size(200, 32);
-            this.rate4.TabIndex = 1;
-            this.rate4.Text = "rate4";
-            this.rate4.Tooltips = new string[] {
-        "terrible",
-        "bad",
-        "normal",
-        "good",
-        "wonderful"};
-            this.rate4.Value = 3F;
-            // 
-            // label5
-            // 
-            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label5.Location = new System.Drawing.Point(3, 245);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(694, 24);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "提示信息";
-            // 
-            // flowPanel2
-            // 
-            this.flowPanel2.Controls.Add(this.rate3);
-            this.flowPanel2.Location = new System.Drawing.Point(3, 201);
-            this.flowPanel2.Name = "flowPanel2";
-            this.flowPanel2.Size = new System.Drawing.Size(694, 38);
-            this.flowPanel2.TabIndex = 14;
-            this.flowPanel2.Text = "flowPanel2";
-            // 
-            // rate3
-            // 
-            this.rate3.AllowHalf = true;
-            this.rate3.Location = new System.Drawing.Point(3, 3);
-            this.rate3.Name = "rate3";
-            this.rate3.Size = new System.Drawing.Size(200, 32);
-            this.rate3.TabIndex = 1;
-            this.rate3.Text = "rate3";
-            this.rate3.Value = 2.5F;
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(3, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(694, 24);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "半星";
             // 
             // flowPanel1
             // 
@@ -251,6 +158,68 @@
             this.label1.TabIndex = 8;
             this.label1.Text = "Rate 评分";
             // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(3, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(694, 24);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "允许清除";
+            // 
+            // flowPanel5
+            // 
+            this.flowPanel5.Controls.Add(this.rate9);
+            this.flowPanel5.Controls.Add(this.rate10);
+            this.flowPanel5.Controls.Add(this.rate11);
+            this.flowPanel5.Location = new System.Drawing.Point(3, 201);
+            this.flowPanel5.Name = "flowPanel5";
+            this.flowPanel5.Size = new System.Drawing.Size(694, 38);
+            this.flowPanel5.TabIndex = 20;
+            this.flowPanel5.Text = "flowPanel5";
+            // 
+            // rate9
+            // 
+            this.rate9.AllowClear = true;
+            this.rate9.Fill = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.rate9.Location = new System.Drawing.Point(415, 3);
+            this.rate9.Name = "rate9";
+            this.rate9.Size = new System.Drawing.Size(200, 32);
+            this.rate9.TabIndex = 2;
+            this.rate9.Text = "rate9";
+            this.rate9.Value = 3F;
+            // 
+            // rate10
+            // 
+            this.rate10.AllowClear = true;
+            this.rate10.Location = new System.Drawing.Point(209, 3);
+            this.rate10.Name = "rate10";
+            this.rate10.Size = new System.Drawing.Size(200, 32);
+            this.rate10.TabIndex = 1;
+            this.rate10.Text = "rate10";
+            this.rate10.Value = 3F;
+            // 
+            // rate11
+            // 
+            this.rate11.AllowClear = true;
+            this.rate11.Count = 3;
+            this.rate11.Location = new System.Drawing.Point(3, 3);
+            this.rate11.Name = "rate11";
+            this.rate11.Size = new System.Drawing.Size(200, 32);
+            this.rate11.TabIndex = 0;
+            this.rate11.Text = "rate11";
+            // 
+            // flowPanel4
+            // 
+            this.flowPanel4.Controls.Add(this.rate8);
+            this.flowPanel4.Controls.Add(this.rate6);
+            this.flowPanel4.Controls.Add(this.rate5);
+            this.flowPanel4.Location = new System.Drawing.Point(3, 423);
+            this.flowPanel4.Name = "flowPanel4";
+            this.flowPanel4.Size = new System.Drawing.Size(694, 38);
+            this.flowPanel4.TabIndex = 26;
+            this.flowPanel4.Text = "flowPanel4";
+            // 
             // rate8
             // 
             this.rate8.Character = "好";
@@ -261,17 +230,108 @@
             this.rate8.Text = "rate8";
             this.rate8.Value = 3F;
             // 
+            // rate6
+            // 
+            this.rate6.Character = "A";
+            this.rate6.Location = new System.Drawing.Point(209, 3);
+            this.rate6.Name = "rate6";
+            this.rate6.Size = new System.Drawing.Size(200, 32);
+            this.rate6.TabIndex = 2;
+            this.rate6.Text = "rate6";
+            this.rate6.Value = 3F;
+            // 
+            // rate5
+            // 
+            this.rate5.Character = resources.GetString("rate5.Character");
+            this.rate5.Location = new System.Drawing.Point(3, 3);
+            this.rate5.Name = "rate5";
+            this.rate5.Size = new System.Drawing.Size(200, 32);
+            this.rate5.TabIndex = 1;
+            this.rate5.Text = "rate5";
+            this.rate5.Value = 3F;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(3, 393);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(694, 24);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "字符自定义";
+            // 
+            // flowPanel3
+            // 
+            this.flowPanel3.Controls.Add(this.rate4);
+            this.flowPanel3.Location = new System.Drawing.Point(3, 349);
+            this.flowPanel3.Name = "flowPanel3";
+            this.flowPanel3.Size = new System.Drawing.Size(694, 38);
+            this.flowPanel3.TabIndex = 24;
+            this.flowPanel3.Text = "flowPanel3";
+            // 
+            // rate4
+            // 
+            this.rate4.Location = new System.Drawing.Point(3, 3);
+            this.rate4.Name = "rate4";
+            this.rate4.Size = new System.Drawing.Size(200, 32);
+            this.rate4.TabIndex = 1;
+            this.rate4.Text = "rate4";
+            this.rate4.Tooltips = new string[] {
+        "terrible",
+        "bad",
+        "normal",
+        "good",
+        "wonderful"};
+            this.rate4.Value = 3F;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(3, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(694, 24);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "提示信息";
+            // 
+            // flowPanel2
+            // 
+            this.flowPanel2.Controls.Add(this.rate3);
+            this.flowPanel2.Location = new System.Drawing.Point(3, 275);
+            this.flowPanel2.Name = "flowPanel2";
+            this.flowPanel2.Size = new System.Drawing.Size(694, 38);
+            this.flowPanel2.TabIndex = 22;
+            this.flowPanel2.Text = "flowPanel2";
+            // 
+            // rate3
+            // 
+            this.rate3.AllowHalf = true;
+            this.rate3.Location = new System.Drawing.Point(3, 3);
+            this.rate3.Name = "rate3";
+            this.rate3.Size = new System.Drawing.Size(200, 32);
+            this.rate3.TabIndex = 1;
+            this.rate3.Text = "rate3";
+            this.rate3.Value = 2.5F;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(3, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(694, 24);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "半星";
+            // 
             // RateDemo
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.Controls.Add(this.stackPanel1);
             this.Name = "RateDemo";
-            this.Size = new System.Drawing.Size(700, 435);
+            this.Size = new System.Drawing.Size(700, 494);
             this.stackPanel1.ResumeLayout(false);
+            this.flowPanel1.ResumeLayout(false);
+            this.flowPanel5.ResumeLayout(false);
             this.flowPanel4.ResumeLayout(false);
             this.flowPanel3.ResumeLayout(false);
             this.flowPanel2.ResumeLayout(false);
-            this.flowPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -286,17 +346,22 @@
         private AntdUI.Rate rate1;
         private AntdUI.Label label3;
         private AntdUI.Rate rate2;
-        private AntdUI.Label label4;
-        private AntdUI.FlowPanel flowPanel2;
-        private AntdUI.Rate rate3;
-        private AntdUI.Label label5;
-        private AntdUI.FlowPanel flowPanel3;
-        private AntdUI.Rate rate4;
+        private AntdUI.Rate rate7;
         private AntdUI.FlowPanel flowPanel4;
+        private AntdUI.Rate rate8;
+        private AntdUI.Rate rate6;
         private AntdUI.Rate rate5;
         private AntdUI.Label label6;
-        private AntdUI.Rate rate6;
-        private AntdUI.Rate rate7;
-        private AntdUI.Rate rate8;
+        private AntdUI.FlowPanel flowPanel3;
+        private AntdUI.Rate rate4;
+        private AntdUI.Label label5;
+        private AntdUI.FlowPanel flowPanel2;
+        private AntdUI.Rate rate3;
+        private AntdUI.Label label4;
+        private AntdUI.FlowPanel flowPanel5;
+        private AntdUI.Rate rate9;
+        private AntdUI.Rate rate10;
+        private AntdUI.Rate rate11;
+        private AntdUI.Label label7;
     }
 }
